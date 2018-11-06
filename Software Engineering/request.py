928abd953a328c1a1acb1edaa88f726358c6a44b
ElliotK134 <13490022@students.lincoln.ac.uk>
2018-11-06 11:35:16 +0000

    File contains solution to task 1
    This file will take a film title and use it to make a request to the ombd api.  It will then return a JSON string

---
+++ Software Engineering/request.py
@@ -0,0 +1,24 @@
+import requests
+
+class movieApi:
+	apiKey = ""
+	url = ""
+	call = ""
+
+	def __init__(self, key, Url):
+		self.apiKey = key
+		self.url = Url
+		self.call = self.url + self.apiKey
+
+	def queryMovie(self, title):
+		response = requests.get(self.call+title)
+		a = response.status_code
+		t = response.text
+		print(a)
+		print(self.call+title)
+		print(t)
+
+
+#"http://www.omdbapi.com/?i=tt3896198&apikey=3db990c4"
+api = movieApi("3db990c4&", "http://www.omdbapi.com/?apikey=")
+api.queryMovie("t="+"Blade runner")
