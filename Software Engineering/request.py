b818bf02bb2c1fcb6072680c9634ae3f837d62b6
Jisha-George <jishathakidiyel@yahoo.com>
2018-11-06 12:51:38 +0000

    interface

--- Software Engineering/request.py
+++ Software Engineering/request.py
@@ -1,4 +1,22 @@
+from PyQt5 import QtWidgets, uic
+
+import json
 import requests
+
+##class interface:
+##        app = QtWidgets.QApplication([])
+##        dlg = uic.loadUi(path)
+##        
+##        def ACME():
+##            dlg.lineEdit_2.setText()
+##                
+##        
+##        def showUI():
+##                self.dlg.show()
+##                self.app.exec()
+##
+##        def __init__(self):
+
 
 class movieApi:
 	apiKey = ""
@@ -19,7 +37,30 @@ class movieApi:
 		print(t)
 		return t
 
+api = movieApi("3db990c4&", "http://www.omdbapi.com/?apikey=")	
+
+def ACME():
+        output = api.queryMovie("t="+"Blade runner")
+        dlg.lineEdit_2.setText(output)
+         
+
+app = QtWidgets.QApplication([])
+dlg = uic.loadUi("ACME.ui")
+
+dlg.pushButton.clicked.connect(ACME)
+
+dlg.show()
+app.exec()
+
 
 #"http://www.omdbapi.com/?i=tt3896198&apikey=3db990c4"
-api = movieApi("3db990c4&", "http://www.omdbapi.com/?apikey=")
-api.queryMovie("t="+"Blade runner")
+##ui = interface()
+##ui.dlg.pushButton.clicked.connect(ui.ACME)
+	
+
+
+
+
+
+
+
